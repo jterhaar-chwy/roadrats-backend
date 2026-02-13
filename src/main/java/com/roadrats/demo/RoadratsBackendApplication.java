59734dc91eabf732351db40b6aefa0a304f21e1c
jterhaar-chwy <jterhaar@chewy.com>
2026-02-13 15:38:41 -0500

    initial commit of spring boot backend

---
+++ src/main/java/com/roadrats/demo/RoadratsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.roadrats.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoadratsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoadratsBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/roadrats/demo/RoadratsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roadrats.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoadratsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
